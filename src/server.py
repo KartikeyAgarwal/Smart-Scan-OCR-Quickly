321fc169d03c7c7b064a35ca5e2527031a43c274
KartikeyAgarwal <2018chb1048@iitrpr.ac.in>
2021-10-01 18:43:26 +0530

    add loader

--- src/server.py
+++ src/server.py
@@ -1,8 +1,6 @@
 from fastapi import FastAPI, Request, UploadFile, File, BackgroundTasks
 from fastapi.templating import Jinja2Templates
 import shutil, os, uuid
-
-from starlette.background import BackgroundTask
 import ocr
 
 app = FastAPI()
@@ -23,26 +21,25 @@ async def perform_ocr(image: UploadFile = File(...)):
 @app.post("/bulk-extract-text")
 async def bulk_perform_ocr(request: Request, bg_task: BackgroundTasks):
     images = await request.form()
-    # print("here")
+    
     folder_name = str(uuid.uuid4())
     os.mkdir(folder_name)
 
     for image in images.values():
         temp_file = _save_file_to_disc(image, path=folder_name, save_as=image.filename)
-        # print("here")
     
     bg_task.add_task(ocr.read_images_from_dir, folder_name, write_to_file=True)
-    print("hello1")
+    
     return {"task_id": folder_name, "num_files": len(images)}
 
 @app.get("/bulk-output/{task_id}")
 async def bulk_output(task_id):
     text_map = {}
     for file_ in os.listdir(task_id):
-        print("here2")
+        
         if file_.endswith("txt"):
             text_map[file_] = open(os.path.join(task_id, file_)).read()
-    print("end")
+    print("DONE")
     return {"task_id": task_id, "output": text_map}
 
 def _save_file_to_disc(uploaded_file, path=".", save_as="default"):
